9d7509e6ccd7b79712e42f3677ea3cbb13d85a92
Patrick Weaver <pjpwea@gmail.com>
2018-05-18 17:57:55 -0400

    Show basic name

--- app/components/ShapeDataGroup.jsx
+++ app/components/ShapeDataGroup.jsx
@@ -36,6 +36,7 @@ class ShapeDataGroup extends React.Component {
     return(
       <ul
         className="datapoint">
+        <li>{this.props.dataGroup.dataGroupName}</li>
         <li>{this.props.dataGroup.dataGroupId}</li>
         <li>Total Sightings: {this.totalSightings()}</li>
         <ul
