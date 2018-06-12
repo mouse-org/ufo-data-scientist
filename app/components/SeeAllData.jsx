e60d2d189f8804dd0382c7b2502b4a3663f33dff
Patrick Weaver <pjpwea@gmail.com>
2018-06-12 17:39:36 -0400

    Show duration data

--- app/components/SeeAllData.jsx
+++ app/components/SeeAllData.jsx
@@ -5,12 +5,22 @@ function SeeAllData(props) {
   const breakApartData = require('../helpers/breakApartData');
 
   const stateData = breakApartData(props.data, "state", "sightings");
+  const durationData = breakApartData(props.data, "duration_minutes", "sightings");
 
   var states = stateData.map((state, index) => {
     return (
       <li>
         {state.state}<br/>
         {state.sightings}<br/>
+      </li>
+    )
+  });
+
+  var durations = durationData.map((duration, index) => {
+    return (
+      <li>
+        {duration.duration_minutes}<br/>
+        {duration.sightings}<br/>
       </li>
     )
   });
@@ -22,6 +32,10 @@ function SeeAllData(props) {
 
       <ol>
         {states}
+      </ol>
+
+      <ol>
+        {durations}
       </ol>
 
     </div>
