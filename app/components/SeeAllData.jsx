c846d7ccb99eed3f15aef64a5d6e9c21159c97dd
Patrick Weaver <pjpwea@gmail.com>
2018-07-10 15:54:17 -0400

    add datetime to see all view

--- app/components/SeeAllData.jsx
+++ app/components/SeeAllData.jsx
@@ -5,6 +5,8 @@ function SeeAllData(props) {
   const breakApartData = require('../helpers/breakApartData');
 
   const stateData = breakApartData(props.data, "state", "sightings");
+  const dateTimeData = breakApartData(props.data, "date_time", "sightings");
+  const shapeData = breakApartData(props.data, "shape", "sightings");
   const durationData = breakApartData(props.data, "duration_minutes", "sightings");
   const latData = breakApartData(props.data, "city_latitude", "sightings");
   const longData = breakApartData(props.data, "city_longitude", "sightings");
@@ -14,6 +16,18 @@ function SeeAllData(props) {
       <li>
         {state.state}<br/>
         {state.sightings}<br/>
+      </li>
+    )
+  });
+
+  var dateTime = dateTimeData.map((dateTime, index) => {
+    console.log(dateTime);
+    console.log(typeof dateTime);
+    console.log("* * * * * * * *")
+    return (
+      <li>
+        {dateTime.date_time}<br/>
+        {dateTime.sightings}<br/>
       </li>
     )
   });
@@ -51,18 +65,23 @@ function SeeAllData(props) {
       <h1>See All Data</h1>
 
       <ol>
+        <h3>States:</h3>
         {states}
       </ol>
-
       <ol>
+        <h3>Datetime:</h3>
+        {dateTime}
+      </ol>
+      <ol>
+        <h3>Durations</h3>
         {durations}
       </ol>
-
       <ol>
+        <h3>Latitudes:</h3>
         {lats}
       </ol>
-
       <ol>
+        <h3>Longitudes:</h3>
         {longs}
       </ol>
 
