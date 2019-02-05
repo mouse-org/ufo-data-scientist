4c0380de11d31ac399d23cd5a64181d1476f0f5f
Patrick Weaver <pjpwea@gmail.com>
2019-02-05 15:57:07 -0500

    get d3 working with react

--- app/app.jsx
+++ app/app.jsx
@@ -17,10 +17,19 @@ const BarChart = require('./components/BarChart')
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this)
     this.state = {
       title: "UFO Data Scientist",
-      data: data
+      //data: data
+      data: [25, 35, 50, 66, 87]
     }
+  }
+  handleChange(event) {
+    let index = (parseInt(event.target.id.substring(7, 8)))
+    const oldData = this.state.data;
+    var newData = oldData;
+    newData[index - 1] = event.target.value;
+    this.setState({data: newData});
   }
 
   componentDidMount() {
@@ -29,22 +38,44 @@ class App extends React.Component {
   componentWillUnmount() {
   }
 
-  extractSet(data, label, transformation = ((i) => i)) {
+  extractProperty(data, label, transformation = (i => i)) {
     const extractedData = data.map(d => d[label])
     return extractedData.map(transformation)
   }
+
+  //consolidateData
 
   //splitIntoCategories(set)
 
   render() {
 
+    //const chartData = this.state.data.map(d => d.duration_minutes)
+
     return (
       <div id="app">
         <h1>{this.state.title}</h1>
+        
+        {/* */}
+        <div id="sliders">
+          <label>1</label>
+          <input type="range" min="1" max="100" value={this.state.data[0]} class="slider" id="slider-1" onChange={this.handleChange}></input>
+          <label>2</label>
+          <input type="range" min="1" max="100" value={this.state.data[1]} class="slider" id="slider-2" onChange={this.handleChange}></input>
+          <label>3</label>
+          <input type="range" min="1" max="100" value={this.state.data[2]} class="slider" id="slider-3" onChange={this.handleChange}></input>
+          <label>4</label>
+          <input type="range" min="1" max="100" value={this.state.data[3]} class="slider" id="slider-4" onChange={this.handleChange}></input>
+          <label>5</label>
+          <input type="range" min="1" max="100" value={this.state.data[4]} class="slider" id="slider-5" onChange={this.handleChange}></input>
+        
+        </div>
+        {/**/}
+        <h3>Data:</h3>
+
         <BarChart
-          data={[24, 26, 38, 41, 56, 69]}
-          x={this.extractSet(data, 'shape')}
-          y={this.extractSet(data, 'duration_minutes')}
+          data={this.state.data}
+          x={this.extractProperty(data, 'shape')}
+          y={this.extractProperty(data, 'duration_minutes')}
         />
       </div>
     )
--- app/components/BarChart.jsx
+++ app/components/BarChart.jsx
@@ -8,30 +8,54 @@ class BarChart extends React.Component {
     //return false
   }
 
+  componentDidUpdate(prevProps) {
+    this.drawChart(this.props.data.map((d,i) => { return {name: i, value: d}}))
+  }
+
+  componentWillReceiveProps() {
+    
+  }
+
   componentDidMount() {
     this.drawChart(this.props.data.map((d,i) => { return {name: i, value: d}}))
   }
 
   drawChart(data) {
+
     var width = 960,
     height = 500;
 
     var margin = ({top: 20, right: 0, bottom: 30, left: 40})
+    function updateChart() {
+      var sel = d3.select("#viz")
+      .selectAll("div")
+      .data(data)
+      .style("width", d => d.value * 10 + 'px')
+      .text(d => d.name + ': ' + d.value)
 
-    d3.select("#viz")
-    .selectAll("div")
-    .data(data)
-    .enter()
-    .append("div")
-    .style("width", d => d.value * 10 + 'px')
-    .text(d => d.name + ': '+ d.value) 
+      sel.enter()
+      .append("div")
+      .style("width", d => d.value * 10 + 'px')
+      .text(d => d.name + ': ' + d.value)
 
+      sel.exit().remove()
+    }
+    updateChart()
 
   }
 
   render() {
+
+    const showData = this.props.data.map((d,i) => 
+      <li>{i}: {d}</li>
+    )
+
     return (
       <div id="container">
+
+        <h3>Data 2:</h3>
+        <ul>{showData}</ul>
+
         <h3>Bar Chart:</h3>
 
         <div id="viz"></div>
